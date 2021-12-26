2bf30e547b85db5d9c9553228baaea8d0e105840
Haffi Mazhar <haff@Haffis-MacBook-Air.local>
2021-12-26 20:00:10 +0000

    updated models, need to make migrations

--- meetups/models.py
+++ meetups/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.db.models.fields import SlugField
 
 # Create your models here.
+class Meetup(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
--- meetups/views.py
+++ meetups/views.py
@@ -1,28 +1,28 @@
 from django.shortcuts import render
 
+from meetups.models import Meetup
+
 # Create your views here.
 
 
 def index(request):
-    meetups = [
-        {"title": "A first meetup", "location": "New York", "slug": "a-first-meetup"},
-        {"title": "A second meetup", "location": "Paris", "slug": "a-second-meetup"},
-    ]
+    meetups = Meetup.objects.all()
     return render(
-        request, "meetups/index.html", {"show_meetups": True, "meetups": meetups}
+        request, "meetups/index.html", {"meetups": meetups}
     )
 
 
 def meetup_details(request, meetup_slug):
-    selected_meetup = {
-        "title": "A first meetup",
-        "description": "This is the first meetup!",
-    }
-    return render(
-        request,
-        "meetups/meetup-details.html",
-        {
-            "meetup_title": selected_meetup["title"],
-            "meetup_description": selected_meetup["description"],
-        },
-    )
+    try:
+        selected_meetup = Meetup.objects.get(slug=meetup_slug)
+        return render(
+            request,
+            "meetups/meetup-details.html",
+            {
+                "meetup_found": True,
+                "meetup_title": selected_meetup.title,
+                "meetup_description": selected_meetup.description,
+            },
+        )
+    except Exception as exc:
+        return render(request, "meetups/meetup-details.html", {"meetup_found": False})
